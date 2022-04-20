d368baf7b3774268fbc820b3a15beb0138d79fec
André Cardoso <andref@ua.pt>
2022-04-21 00:27:39 +0200

    finished work with react

--- students-pack/FontChooser.js
+++ students-pack/FontChooser.js
@@ -1,21 +1,158 @@
+
 class FontChooser extends React.Component {
 
-    constructor(props) {
-	super(props);
-    }
-    
+	constructor(props) {
+		super(props);
+		if (this.props.bold == 'true') {
+			this.default_bold = this.props.bold;
+		}
+		else {
+			this.default_bold
+		}
+		this.Size = this.props.size;
+		this.text_showing = false;
+		this.min=this.props.min
+		this.max=this.props.max
+	}
 
-    render() {
+	componentDidMount() {
+		if (this.props.bold == 'true') {
+			textSpan.style.fontWeight = "bold";
+		}
+		else if (this.props.bold == 'false') {
+			textSpan.style.fontWeight = "normal";
+		}
+		console.log(this.default_bold)
 
-	return(
-	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
-	       </div>
-	);
-    }
+		if(this.props.min<=0){
+			this.min=1
+		}
+		if(this.props.min>this.props.max){
+			this.max=400000000
+			this.Size=this.props.min
+			textSpan.style.fontSize = this.Size + "px"
+			fontSizeSpan.innerText = this.Size;
+			textSpan.style.color = "red"
+		}
+		if(this.props.size<this.props.min){
+			this.Size=this.min
+			textSpan.style.fontSize = this.Size + "px"
+			fontSizeSpan.innerText = this.Size;
+			textSpan.style.color = "red"
+		}
+
+		if(this.props.size>this.props.max){
+			this.Size=this.max
+			textSpan.style.fontSize = this.Size + "px"
+			fontSizeSpan.innerText = this.Size;
+			textSpan.style.color = "red"
+		}
+	};
+
+	toggleChange = () => {
+		console.log(boldCheckbox.checked)
+		if (boldCheckbox.checked) {
+			textSpan.style.fontWeight = "bold"
+		}
+		else {
+			textSpan.style.fontWeight = "normal"
+		}
+	};
+
+	Decrease = () => {
+		if (this.Size > parseInt(this.min, 10)) {
+			this.Size--
+			textSpan.style.fontSize = this.Size + "px"
+			fontSizeSpan.innerText = this.Size;
+			console.log(textSpan.style.fontSize)
+			this.Limits_Color()
+		}
+	}
+
+	Increase = () => {
+		if (this.Size < parseInt(this.max, 10)) {
+			this.Size++
+			textSpan.style.fontSize = this.Size + "px"
+			fontSizeSpan.innerText = this.Size;
+			console.log(textSpan.style.fontSize)
+			this.Limits_Color()
+		}
+	}
+
+	Limits_Color = () => {
+		if (this.Size == parseInt(this.min, 10) || this.Size == parseInt(this.max, 10)) {
+			console.log("On the limits")
+			textSpan.style.color = "red"
+		}
+		else {
+			textSpan.style.color = "black"
+		}
+	}
+
+	Show_items = (e) => {
+		var click =e.detail;
+		const timer = setTimeout(() => {
+
+			if (click === 1) {
+				console.log("click");
+				if (this.text_showing == false) {
+					boldCheckbox.hidden = false;
+					decreaseButton.hidden = false;
+					fontSizeSpan.hidden = false;
+					increaseButton.hidden = false;
+					this.text_showing = true
+				}
+
+				else {
+					boldCheckbox.hidden = true;
+					decreaseButton.hidden = true;
+					fontSizeSpan.hidden = true;
+					increaseButton.hidden = true;
+					this.text_showing = false
+					click=0
+					e.detail=0
+				}
+
+			}
+		}, 250);
+
+		if (click === 2) {
+			console.log("double click");
+			clearTimeout(timer);
+			this.Size = this.props.size;
+			textSpan.style.fontSize = this.Size + "px"
+			fontSizeSpan.innerText = this.Size;
+			textSpan.style.color = "black"
+			click=0
+			e.detail=0
+		}
+	};
+
+
+	render() {
+		function declare() {
+			let boldCheckbox = document.getElementById("boldCheckbox");
+			let decreaseButton = document.getElementById("decreaseButton");
+			let fontSizeSpan = document.getElementById("fontSizeSpan");
+			let increaseButton = document.getElementById("increaseButton");
+			let textSpan = document.getElementById("textSpan");
+		};
+
+
+
+
+		return (
+			<div>
+				{declare()
+
+				}
+				<input onChange={this.toggleChange} defaultChecked={this.default_bold} type="checkbox" id="boldCheckbox" hidden={true} />
+				<button id="decreaseButton" onClick={this.Decrease} hidden={true}>-</button>
+				<span id="fontSizeSpan" hidden={true}>{this.Size}</span>
+				<button id="increaseButton" onClick={this.Increase} hidden={true}>+</button>
+				<span onClick={this.Show_items} id="textSpan">{this.props.text}</span>
+			</div>
+		);
+	}
 }
 
